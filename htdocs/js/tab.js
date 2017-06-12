f567b6bf6a59ee0925f46fa0fab3fd2e882fe347
葦澤 紀美子 <ashizawa@contents.ne.jp>
2017-06-12 15:56:25 +0900

    First commit

---
+++ htdocs/js/tab.js
@@ -0,0 +1,9 @@
+  $(function() {
+      $(".tab li").click(function() {
+          var num = $(".tab li").index(this);
+          $(".tabContent").removeClass('active');
+          $(".tabContent").eq(num).addClass('active');
+          $(".tab li").removeClass('active');
+          $(this).addClass('active')
+      });
+  });
